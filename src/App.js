c44c94f4c8fc5e4ea94b02f1d67d9b389d8dec54
Akshad Jaiswal <111684658+akshadjaiswal@users.noreply.github.com>
2022-08-30 14:42:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var flagDictionary = {
+  "🏁": "Chequered Flag",
+  "🚩": "Triangular Flag",
+  "🎌": "Crossed Flag",
+  "🏴 ": "Black Flag",
+  "🏳️": "White Flag",
+  "🏳️‍🌈": "Rainbow Flag",
+  "🏳️‍⚧️": "Transgender Flag",
+  "🏴‍☠️": "Pirate Flag"
+};
+var flagWeKnow = Object.keys(flagDictionary);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  function flagInputHandler(event) {
+    var userInput = event.target.value;
+
+    var meaning = flagDictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning = "we dont have this in our database";
+    }
+    setMeaning(meaning);
+  }
+  function flagClickHandler(emoji) {
+    var meaning = flagDictionary[emoji];
+    setMeaning(meaning);
+  }
+  return (
+    <div className="App">
+      <h1>Flag Interpreter</h1>
+      <input onChange={flagInputHandler} />
+      <h2> {meaning}</h2>
+      <h3>Flags we know </h3>
+      {flagWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => flagClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
